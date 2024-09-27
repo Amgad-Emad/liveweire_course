813373c12682d31ec5c7ddb9cf1700a8ebf7199e
Amgad-Emad <amgademad447@gmail.com>
2024-09-27 22:55:02 +0300

    vid1 Examples

---
+++ app/Livewire/Clicker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Clicker extends Component
+{
+    // ------------------------------------- first video example
+                // public function clickHandel(){
+                //     dd('clicked');
+                // }
+
+    // ------------------------------------- second video example
+
+
+
+    public function render()
+    {
+        return view('livewire.clicker');
+    }
+}
---
+++ resources/views/livewire/clicker.blade.php
@@ -0,0 +1,12 @@
+{{-- the first video example--}}
+
+
+
+<div>
+    {{-- first in the video  --}}
+    {{-- hello form livewire course --}}
+    {{-- second in the video --}}
+    <button wire:click="clickHandel">
+        click me
+    </button>
+</div>
